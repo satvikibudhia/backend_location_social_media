3d14a90dbcb8709b194dae0a31300e94573f834a
yiungyiung <yashparkhi.super@gmail.com>
2024-11-10 02:54:45 +0530

    sad

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: "*",
+        origin: "http://localhost:3000",
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
         allowedHeaders: ["Content-Type", "Authorization"],
